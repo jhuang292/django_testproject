39a514f39d53dc170d06d5e3e758d5fcbede89aa
jhuang292 <jhuang292@wisc.edu>
2019-05-28 15:08:10 -0400

    update

---
+++ pollProVenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\owner\desktop\summitworks_code\django_poll_project\pollprovenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
