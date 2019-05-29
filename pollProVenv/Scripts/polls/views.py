563a339d97404284e0eaeb594c1ac1c2d492a678
jhuang292 <jhuang292@wisc.edu>
2019-05-29 15:24:46 -0400

    updated

--- pollProVenv/Scripts/polls/views.py
+++ pollProVenv/Scripts/polls/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.models import User
 
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 
-from django.views import generic
 
+from django.views import generic
 from .models import Question, Choice
 
 from django.http import Http404
@@ -12,7 +13,6 @@ from django.http import Http404
 from django.utils import timezone
 
 # Create your views here.
-
 class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
 	context_object_name = 'latest_question_list'
@@ -37,14 +37,15 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
 
 	p = get_object_or_404(Question, pk=question_id)
-	try:
-		selected_choice = p.choice_set.get(pk=request.POST['choice'])
-	except (KeyError, Choice.DoesNotExist):
-		context = {'question':p, 'error_message': 'You didn\'t select a choice yet.'}
-		return render(request, 'polls/detail.html', context)
-	else:
-		selected_choice.votes += 1
-		selected_choice.save()
-		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+	if request.user.is_authenticated:
+		try:
+			selected_choice = p.choice_set.get(pk=request.POST['choice'])
+		except (KeyError, Choice.DoesNotExist):
+			context = {'question':p, 'error_message': 'You didn\'t select a choice yet.'}
+			return render(request, 'polls/detail.html', context)
+		else:
+			selected_choice.votes += 1
+			selected_choice.save()
+			return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 
--- pollProVenv/Scripts/testproject/urls.py
+++ pollProVenv/Scripts/testproject/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView # new
+
 
 urlpatterns = [
     url(r'^admin/',admin.site.urls),
     url(r'^polls/',include(('polls.urls','polls'),namespace='polls')),
+    url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'', TemplateView.as_view(template_name='home.html'), name='home'), # new
 ]
+
